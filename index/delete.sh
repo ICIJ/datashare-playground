8f15ec9e09cdfba8e26f80a8d63d93fd0e349771
Pierre Romera <hello@pirhoo.com>
2023-06-01 13:10:12 +0200

    feat: add script to delete index

---
+++ index/delete.sh
@@ -0,0 +1,19 @@
+#!/bin/bash -e
+
+script_dir=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+source $script_dir/../lib/cli.sh
+source $script_dir/../lib/colors.sh
+
+check_usage 1 '<index>'
+check_env
+check_bins
+check_elasticsearch_url
+
+echo -e "${BRed}This action cannot be undone 💣${Color_Off}" 
+read -p "To confirm the deletion, please enter the name of the index again: " choice
+case "$choice" in 
+  $1 ) curl -XDELETE "$ELASTICSEARCH_URL/$1" | jq;;
+  * ) echo "Invalid input: deletion aborted.";;
+esac
+
+# 
