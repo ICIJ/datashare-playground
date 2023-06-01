663e667c91c5bd422154282b6b8475cb77c4fd94
Pierre Romera <hello@pirhoo.com>
2023-06-01 12:35:42 +0000

    feat: add query string argument

--- document/count.sh
+++ document/count.sh
@@ -3,18 +3,25 @@
 script_dir=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 source $script_dir/../lib/cli.sh
 
-check_usage 2 '<index> <path>'
+check_usage 1 '<index> [<path>] [<query_string>]'
 check_bins
 check_env
 check_elasticsearch_url
 
 index=$1
-path=${2%/};
+path=${2:-/}
+path=${path%/}
+query_string=${3:-'*:*'}
 
 body='{
   "query": {
     "bool" : {
       "must" : [
+        {
+          "query_string": {
+            "query": "'"${query_string}"'" 
+          }
+        },
         {
           "prefix": { 
             "path": "'"${path}"'"
