0ef9bbbeafb289f758f6db12b73dc27cf0b89fe2
Pierre Romera <hello@pirhoo.com>
2023-06-06 12:26:14 +0200

    feat: add "Duplicate" scripts

---
+++ duplicate/count.sh
@@ -0,0 +1,33 @@
+#!/bin/bash -e
+
+script_dir=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+source $script_dir/../lib/cli.sh
+
+check_usage 1 '<index> [<query_string>]'
+check_bins
+check_env
+check_elasticsearch_url
+
+index=$1
+query_string=${2:-'*:*'}
+
+body='{
+  "query": {
+    "bool" : {
+      "must" : [
+        {
+          "query_string": {
+            "query": "'"${query_string}"'" 
+          }
+        },
+        {
+          "term" : { 
+            "type" : "Duplicate" 
+          }
+        }
+      ]
+    }
+  }
+}'
+
+curl -sXPOST "$ELASTICSEARCH_URL/$index/_count" -H 'Content-Type: application/json' -d "$body" | jq '.count'
