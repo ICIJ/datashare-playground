c16f1ceefc237c982aff8785afc7ee7e4260557e
Pierre Romera <hello@pirhoo.com>
2023-06-06 12:26:53 +0200

    feat: add script to refresh index

---
+++ index/refresh.sh
@@ -0,0 +1,14 @@
+#!/bin/bash -e
+
+script_dir=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+source $script_dir/../lib/cli.sh
+
+check_usage 1 '<index>'
+check_env
+check_bins
+check_elasticsearch_url
+
+index=$1
+esindex=$ELASTICSEARCH_URL/$index
+
+curl -sXPOST  "$esindex/_refresh" | jq
